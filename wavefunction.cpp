8e315aa154a3e45ebd2866c23f55b3963517ae5a
m0baxter <m0baxter@gmail.com>
2013-11-08 17:05:40 -0500

    Various changes.
    Modified data members and functions to avoid
    reproducing code.
    
    Added various helper functions to simplify code.
    
    Should be almost ready to begin modifiying the
    correlation integral functions.

--- wavefunction.cpp
+++ wavefunction.cpp
@@ -1,25 +1,38 @@
+/*
+T = 0
+P = 1
+D = 1
+G = 3
+
+T + D = 1
+P + D = 2
+T + G = 3
+P + G = 4
+*/
 
 #include "wavefunction.hpp"
 
 const double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982;
 
-WaveFunction::WaveFunction( char* inputpath , int energy ) {
+WaveFunction::WaveFunction( char* inputpath , int energy, bool wb ) {
    /*Constructor for objects of class WaveFunction.*/
 
    //initialize tables:
    readlattice();
-   readradial('T');
-   readradial('P');
-   readradial_gs('T');
-   readradial_gs('P');
+   readradial('T', 'D');
+   readradial('P', 'D');
+   readradial('T'. 'G');
+   readradial('P', 'G');
    readamplitudes( 'T', energy );
    readamplitudes( 'P', energy );
    readinput( 'T', inputpath );
    readinput( 'P', inputpath );
 
    //Calculate table of integrals
-   generate_integral_table();
-
+   if (!wb){
+      generate_integral_table('T');
+      generate_integral_table('P');
+   }
 }
 
 
@@ -28,6 +41,18 @@ WaveFunction::~WaveFunction() {
 
    delete [] terms;
    delete [] factors;
+}
+
+
+int WaveFunction::TP_toint( const char c ) {
+
+   return ( 84 - (c + 0) )/4;
+}
+
+
+int WaveFunction::DG_toint( const char c ) {
+
+   return ( (c + 0) - 68 )/3;
 }
 
 
@@ -72,17 +97,20 @@ void WaveFunction::readlattice() {
 }
 
 
-void WaveFunction::readradial( const char centre ) {
+void WaveFunction::readradial( const char centre, const char type ) {
    /*Reads/stores the R_{nl} function data.*/
 
    std::string path;
    std::string line;
+   
+   int c = TP_toint(centre);
+   int t = DG_toint(type);
 
    for (int n = 1; n < 5; ++n) {
       for (int l = 0; l < n; ++l) {
       
-         switch (centre) {
-            case 'T':
+         switch (centre + type) {
+            case 'T' + 'D': //target dynamic
                //path = "/home/baxter/Documents/Observable/Input/radial/silverman_1s1s/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/silverman full/R_";
                  path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-2p/R_";
@@ -91,8 +119,9 @@ void WaveFunction::readradial( const char centre ) {
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4d/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4d(all)/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4f/R_";
+               break;
                
-            case 'P':
+            case 'P' + 'D': //projectile dynamic
                //path = "/home/baxter/Documents/Observable/Input/radial/silverman_1s1s/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/silverman full/R_";
                  path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-2p/R_";
@@ -101,6 +130,33 @@ void WaveFunction::readradial( const char centre ) {
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4d/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4d(all)/R_";
                //path = "/home/baxter/Documents/Observable/Input/radial/MCHF_1s-4f/R_";
+               break;
+               
+            case 'T' + 'G': //target ground state
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_1s1sp/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_full/R_";
+                path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-2p/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s(all)/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d(all)/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4f/R_";
+               break;
+               
+            case 'P' + 'G': //projectile ground state
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_1s1sp/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_full/R_";
+                path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-2p/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s(all)/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d(all)/R_";
+               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4f/R_";
+               break;
+               
+            default:
+               std::cout << "readradial error: Improper centre/type signifier" << std::endl;
+               return;
          }
 
          std::stringstream ss;
@@ -116,14 +172,8 @@ void WaveFunction::readradial( const char centre ) {
 
          delete[] pth;
 
-         //Give R_nl^centre(0) some value (doesn't really matter what it is as long as its not zero:
-         switch (centre) {
-            case 'T':
-               Rad_T[n][l][0] = 1.0;
-            case 'P':
-               Rad_P[n][l][0] = 1.0;
-         }
-
+         //Give R_nl(0) some value (doesn't really matter what it is as long as its not zero):
+         Rad[c][t][n][l][0] = 1.0;
 
          for (int i = 1; i < LEN; ++i) {
             //get a line
@@ -132,80 +182,7 @@ void WaveFunction::readradial( const char centre ) {
             ssline << line;
 
             //Store:
-            switch (centre) {
-               case 'T':
-                  ssline >> Rad_T[n][l][i];
-               case 'P':
-                  ssline >> Rad_P[n][l][i];
-            }
-
-         }
-         readfile.close();
-      }
-   }
-}
-
-
-void WaveFunction::readradial_gs() {
-   /*Reads/stores the R_{nl} function data.*/
-
-   std::string path;
-   std::string line;
-
-   for (int n = 1; n < 5; ++n) {
-      for (int l = 0; l < n; ++l) {
-      
-         switch (centre) {
-            case 'T':
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_1s1sp/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_full/R_";
-                path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-2p/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s(all)/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d(all)/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4f/R_";
-               
-            case 'P':
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_1s1sp/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/silverman_full/R_";
-                path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-2p/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4s(all)/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4d(all)/R_";
-               //path = "/home/baxter/Documents/Observable/Input/radial/Ground state/MCHF_1s-4f/R_";
-         }
-
-         std::stringstream ss;
-         ss << n << l << ".txt";
-         path += ss.str();
-
-         //convert string to char*:
-         char* pth = new char[path.size()+1];
-         strcpy(pth,path.c_str());
-
-         //Open file:
-         std::ifstream readfile(pth);
-
-         delete[] pth;
-
-         //Give R_nl(0) some value (doesn't really matter what it is as long as its not zero:
-         Rad_gs[n][l][0] = 1.0;
-
-         for (int i = 1; i < LEN; ++i) {
-            //get a line
-            getline(readfile,line);
-            std::stringstream ssline;
-            ssline << line;
-
-            //Store:
-            switch (centre) {
-               case 'T':
-                  ssline >> Rad_gs_T[n][l][i];
-               case 'P':
-                  ssline >> Rad_gs_P[n][l][i];
-            }
+            ssline >> Rad[c][t][n][l][i];
 
          }
          readfile.close();
@@ -222,6 +199,8 @@ void WaveFunction::readamplitudes( const char centre, int energy ) {
    char* tok1;
    char* tok2;
    
+   int c = TP_toint(centre);
+   
    //setup the path string:
    switch (centre) {
       case 'T':
@@ -229,12 +208,18 @@ void WaveFunction::readamplitudes( const char centre, int energy ) {
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/OPM/E";
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/MCHF_1s4d/E";
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/MCHF_1s4d(all)/E";
+         break;
 
       case 'P':   
            std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/old/E";
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/OPM/E";
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/MCHF_1s4d/E";
          //std::string path = "/home/baxter/Documents/Observable/Input/amplitudes/MCHF_1s4d(all)/E";   
+         break;
+         
+      default:
+         std::cout << "readamplitudes error: Improper centre label" << std::endl;
+         return;
    }
 
    ss << energy << ".txt";
@@ -295,12 +280,8 @@ void WaveFunction::readamplitudes( const char centre, int energy ) {
                im >> impart;
 
                //convert to complex(tok1,tok2) and store in amps[i][n][l][m].
-               switch (centre) {
-                  case 'T':
-                     amps_T[i][n][l][m] = std::complex<double>( realpart, impart );
-                  case 'P':
-                     amps_P[i][n][l][m] = std::complex<double>( realpart, impart );
-               }
+               amps[c][i][n][l][m] = std::complex<double>( realpart, impart );
+
             }
          }
       }
@@ -364,9 +345,14 @@ void WaveFunction::readinput( const char, char* inputpath ) {
       case 'T':
          terms_T = temp_t;
          factors_T = temp_f;
+         break;
       case 'P':
          terms_P = temp_t;
          factors_P = temp_f;
+         break;
+      default:
+         std::cout << "readinput error: Improper centre label" << std::endl;
+         return;
    }
 
    readfile.close();
@@ -388,27 +374,16 @@ int WaveFunction::r_index( double x) {
 }
 
 
-double WaveFunction::R( const char centre, int n, int l, double x) {
+double WaveFunction::R( const char centre, const char type, int n, int l, double x) {
    /*Returns the value of the R_nl function evaluated at x. x must be in r (ie a lattice point).*/
    
-   switch (centre) {
-      case 'T':
-         return Rad_T[n][l][r_index(x)];
-      case 'P':
-         return Rad_P[n][l][r_index(x)];
-   }
+   return Rad[TP_toint(centre)][DG_toint(type)][n][l][r_index(x)];
 }
 
-
-double WaveFunction::R_gs( const char centre, int n, int l, double x) {
-   /*Returns the value of the R_nl function evaluated at x. x must be in r (ie a lattice point).*/
+std::complex<double> WaveFunction::a( const char centre, int i, int n, int l, int m ) {
+   /*Return the coefficient a_centre[n][l][m].*/
    
-   switch (centre) {
-      case 'T':
-         return Rad_gs_T[n][l][r_index(x)];
-      case 'P':
-         return Rad_gs_P[n][l][r_index(x)];
-   }
+   return amps[TP_toint(centre)][i][n][l][m];
 }
 
 
@@ -419,7 +394,24 @@ double WaveFunction::Hep1( double x ) {
 }
 
 
-double* WaveFunction::integrand(int n1, int l1, int n2, int l2 , int s_n_1 , int s_l_1, int s_n_2 , int s_l_2) {
+double WaveFunction::indi_electron( const char centre, int i ) {
+   /*Calculates the indipendent electron model ionization probability (p_centre)
+   for impact parameter b[i].*/
+
+   std::complex<double> p = std::complex<double>(0.0,0.0);
+
+   for (int n = 1; n < MAXn; ++n) {
+      for (int l = 0; l < n; ++l) {
+         for (int m = 0; m < 2*l + 1; ++m) {
+                  p += a(centre,i,n,l,m) * conj( a(centre,i,n,l,m) );
+         }
+      }
+   }
+   return  real( p );
+}
+
+
+double* WaveFunction::integrand(const char c, int n1, int l1, int n2, int l2 , int s_n_1 , int s_l_1, int s_n_2 , int s_l_2) {
    /*Generates the values of the integrand for the given values.*/
 
    static double result[LIMIT];
@@ -428,14 +420,14 @@ double* WaveFunction::integrand(int n1, int l1, int n2, int l2 , int s_n_1 , int
 
    for (int i = 1; i < LIMIT; ++i) {
 
-      result[i] = r[i]*r[i] * R_gs( s_n_1 , s_l_1, r[i]) * R_gs( s_n_2 , s_l_2, r[i]) * ( R(n1,l1,r[i])/R(1,0,r[i]) ) * ( R(n2,l2,r[i])/R(1,0,r[i]) );
+      result[i] = r[i]*r[i] * R(c,'G', s_n_1 , s_l_1, r[i]) * R(c,'G', s_n_2 , s_l_2, r[i]) * ( R(c,'D',n1,l1,r[i])/R(c,'D',1,0,r[i]) ) * ( R(c,'D',n2,l2,r[i])/R(c,'D',1,0,r[i]) );
    }
 
    return result;
 }
 
 
-double* WaveFunction::integrand_wb( double N, int n1, int l1, int n2, int l2 , int s_n_1 , int s_l_1, int s_n_2 , int s_l_2) {
+double* WaveFunction::integrand_wb( const char c, double N, int n1, int l1, int n2, int l2 , int s_n_1 , int s_l_1, int s_n_2 , int s_l_2) {
    /*Generates the values of the WB integrand for the given values.*/
 
    static double result[LIMIT];
@@ -444,15 +436,17 @@ double* WaveFunction::integrand_wb( double N, int n1, int l1, int n2, int l2 , i
 
    for (int i = 1; i < LIMIT; ++i) {
 
-      result[i] = r[i]*r[i] * R_gs( s_n_1 , s_l_1, r[i]) * R_gs( s_n_2 , s_l_2, r[i]) * ( R(n1,l1,r[i])  * R(n2,l2,r[i]) / ( (2 - N) * Hep1(r[i])*Hep1(r[i]) + 2.0 * (N - 1) * R(1,0,r[i]) * R(1,0,r[i]) ) );
+      result[i] = r[i]*r[i] * R(c,'G', s_n_1 , s_l_1, r[i]) * R(c,'G', s_n_2 , s_l_2, r[i]) * ( R(c,'D',n1,l1,r[i])  * R(c,'D',n2,l2,r[i]) / ( (2 - N) * Hep1(r[i])*Hep1(r[i]) + 2.0 * (N - 1) * R(c,'D',1,0,r[i]) * R(c,'D',1,0,r[i]) ) );
    }
 
    return result;
 }
 
 
-void WaveFunction::generate_integral_table() {
+void WaveFunction::generate_integral_table( const char centre ) {
    /*Fills the integral table with integrals.*/
+   
+   int c = TP_toint(centre);
 
    for (int n1 = 1; n1 < 5; ++n1) {
       for (int l1 = 0; l1 < n1; ++l1) {
@@ -463,10 +457,10 @@ void WaveFunction::generate_integral_table() {
                      for (int n2_gs = n1_gs; n2_gs < 5; ++n2_gs) {
                         for (int l2_gs = 0; l2_gs < n2_gs; ++l2_gs) {
 
-                           int_table[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs] = newtoncotes(r,integrand( n1, l1, n2, l2, n1_gs, l1_gs, n2_gs, l2_gs), LIMIT);
-                           int_table[n2][l2][n1][l1][n1_gs][l1_gs][n2_gs][l2_gs] = int_table[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
-                           int_table[n1][l1][n2][l2][n2_gs][l2_gs][n1_gs][l1_gs] = int_table[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
-                           int_table[n2][l2][n1][l1][n2_gs][l2_gs][n1_gs][l1_gs] = int_table[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs] = newtoncotes(r,integrand( centre, n1, l1, n2, l2, n1_gs, l1_gs, n2_gs, l2_gs), LIMIT);
+                           int_table[c][n2][l2][n1][l1][n1_gs][l1_gs][n2_gs][l2_gs] = int_table[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table[c][n1][l1][n2][l2][n2_gs][l2_gs][n1_gs][l1_gs] = int_table[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table[c][n2][l2][n1][l1][n2_gs][l2_gs][n1_gs][l1_gs] = int_table[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
 
                         }
                      }
@@ -479,8 +473,10 @@ void WaveFunction::generate_integral_table() {
 }
 
 
-void WaveFunction::generate_integral_table_wb( double N  ) {
+void WaveFunction::generate_integral_table_wb( const char centre, double N  ) {
    /*Fills the WB integral table with integrals.*/
+   
+   int c = TP_toint(centre);
 
    for (int n1 = 1; n1 < 5; ++n1) {
       for (int l1 = 0; l1 < n1; ++l1) {
@@ -491,10 +487,10 @@ void WaveFunction::generate_integral_table_wb( double N  ) {
                      for (int n2_gs = n1_gs; n2_gs < 5; ++n2_gs) {
                         for (int l2_gs = 0; l2_gs < n2_gs; ++l2_gs) {
 
-                           int_table_wb[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs] = newtoncotes(r,integrand_wb( N, n1, l1, n2, l2, n1_gs, l1_gs, n2_gs, l2_gs), LIMIT);
-                           int_table_wb[n2][l2][n1][l1][n1_gs][l1_gs][n2_gs][l2_gs] = int_table_wb[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
-                           int_table_wb[n1][l1][n2][l2][n2_gs][l2_gs][n1_gs][l1_gs] = int_table_wb[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
-                           int_table_wb[n2][l2][n1][l1][n2_gs][l2_gs][n1_gs][l1_gs] = int_table_wb[n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table_wb[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs] = newtoncotes(r,integrand_wb( centre, N, n1, l1, n2, l2, n1_gs, l1_gs, n2_gs, l2_gs), LIMIT);
+                           int_table_wb[c][n2][l2][n1][l1][n1_gs][l1_gs][n2_gs][l2_gs] = int_table_wb[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table_wb[c][n1][l1][n2][l2][n2_gs][l2_gs][n1_gs][l1_gs] = int_table_wb[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
+                           int_table_wb[c][n2][l2][n1][l1][n2_gs][l2_gs][n1_gs][l1_gs] = int_table_wb[c][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
                         }
                      }
                   }
@@ -506,20 +502,10 @@ void WaveFunction::generate_integral_table_wb( double N  ) {
 }
 
 
-double WaveFunction::indi_electron( int i ) {
-   /*Calculates the indipendent electron model ionization probability for impact parameter b[i].*/
-
-   std::complex<double> p = std::complex<double>(0.0,0.0);
-
-   for (int n = 1; n < MAXn; ++n) {
-      for (int l = 0; l < n; ++l) {
-         for (int m = 0; m < 2*l + 1; ++m) {
-            p += amps[i][n][l][m] * conj( amps[i][n][l][m] );
-         }
-      }
-   }
-
-   return  real( 1.0 - p );
+double table( const char centre, int n1, int l1, int n2, int l2 , int n1_gs , int l1_gs, int n2_gs , int l2_gs ) {
+   /*Returns the integral for the given radial function identifiers on centre.*/
+   
+   return int_table[TP_toint(centre)][n1][l1][n2][l2][n1_gs][l1_gs][n2_gs][l2_gs];
 }
 
 
@@ -612,12 +598,11 @@ double  WaveFunction::correlationintegral( int a ) {
          }
       }
    }
-
    return   32.0 * PI * PI * real( Ic ) ;
 }
 
 
-double  WaveFunction::correlationintegral_wb( int a ) {
+double  WaveFunction::correlationintegral_wb( const char c, int a ) {
    /*Calculates the correlation integral for the impact parameter b[i].*/
    
    //Holds the result:
@@ -717,7 +702,6 @@ double  WaveFunction::correlationintegral_wb( int a ) {
             }
          }
       }
-
       return   128.0 * ( N - 1 ) * PI * PI * real( Ic );
    }
 }
--- wavefunction.hpp
+++ wavefunction.hpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <math.h>
 #include <complex>
+#include <limits>
 #include <stdlib.h>
 #include "newtoncotes.hpp"
 #include "AngularMomentum.hpp"
@@ -22,12 +23,12 @@
 class WaveFunction {
 
    public:
-      WaveFunction( char* , int );
+      WaveFunction( char* , int, wb );
       ~WaveFunction();
       float get_impact( int );
-      double indi_electron( int );
-      double correlationintegral( int );
-      double correlationintegral_wb( int );
+      double indi_electron( const char, int );
+      double correlationintegral( const char, int );
+      double correlationintegral_wb( const char, int );
 
    private:
       //Variables:
@@ -35,46 +36,42 @@ class WaveFunction {
       static const int LEN = 1601;
       static const int LIMIT = 1601; //1001;
       double r[LEN]; //Lattice points for the radial R_nl functions.
-      double Rad_T[5][4][LEN];  //The R_nl function values on target.
-      double Rad_P[5][4][LEN];  //The R_nl function values on projectile.
-      double Rad_gs_T[5][4][LEN];  //The ground state R_nl function values on target.
-      double Rad_gs_P[5][4][LEN];  //The ground state R_nl function values on projectile.
+      double Rad[2][2][5][4][LEN];  //The R_nl function values on target.
       float b[30];  //holds the values of the impact parameter.
-      std::complex<double> amps_T[30][5][4][7];  //Holds the amplitude values for the 30 impact parameters on the target.
-      std::complex<double> amps_P[30][5][4][7];  //Holds the amplitude values for the 30 impact parameters.
+      std::complex<double> amps[2][30][5][4][7];  //Holds the amplitude values for the 30 impact parameters on the target.
 
-      double int_table_T[5][4][5][4][5][4][5][4]; //Holds the integral table.
-      double int_table_P[5][4][5][4][5][4][5][4]; //Holds the integral table.
-      double int_table_wb_T[5][4][5][4][5][4][5][4]; //Holds the integral table for the Wilken and Bauer version.
-      double int_table_wb_P[5][4][5][4][5][4][5][4]; //Holds the integral table for the Wilken and Bauer version.
+      double int_table[2][5][4][5][4][5][4][5][4]; //Holds the integral table.
+      double int_table_wb[2][5][4][5][4][5][4][5][4]; //Holds the integral table for the Wilken and Bauer version.
 
       int N_terms_T;  //The number of terms in the wave function on target.
-      int N_terms_P;  //The number of terms in the wave function on projectile.
       int (*terms_T)[2][3];  //Holds the quantum numbers of the terms of the wave function.
-      int (*terms_P)[2][3];  //Holds the quantum numbers of the terms of the wave function.      
       double* factors_T;  //Holds the prefactors for each term of the wave function.
+      int N_terms_P;  //The number of terms in the wave function on projectile.
+      int (*terms_P)[2][3];  //Holds the quantum numbers of the terms of the wave function.
       double* factors_P;  //Holds the prefactors for each term of the wave function.      
 
       //Functions:
+      int TP_toint( const char );
+      int DG_toint( const char );
       void readlattice();
-      void readradial( const char );
-      void readradial_gs( const char );
-      void readamplitudes_T( const char, int );
+      void readradial( const char, const char );
+      void readamplitudes( const char, int );
       void readinput( const char, char* );
       int r_index( double );
-      
-      
-      double R( const char, int , int , double );
-      double R_gs( const char, int , int , double );
+      double R( const char, const char, int , int , double );
       double Hep1( double );
+      std::complex<double> a( const char, int , int, int, int );
+      double table( const char, int, int, int, int, int, int, int, int );
       
             //Here
       
       //Modify these later:
-      double* integrand(int , int , int , int , int , int , int , int );
-      void generate_integral_table();
-      double* integrand_wb( double , int , int , int , int , int , int , int , int );
-      void generate_integral_table_wb( double );
+      double* integrand( const char, int , int , int , int , int , int , int , int );
+      void generate_integral_table( const, char );
+      double* integrand_wb( const char, double , int , int , int , int , int , int , int , int );
+      void generate_integral_table_wb( const char, double );
+      
+      // add TP integral functions
 
 };
 
