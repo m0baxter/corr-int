ea26f13efdf16674cd5f54216c6936353ceb5452
m0baxter <m0baxter@gmail.com>
2013-11-11 16:52:24 -0500

    Everything but Ic_TP
    Adapted correlation integral functions
    
    Modified main() to match
    
    Reproduces old results prefectly

--- makefile
+++ makefile
@@ -4,18 +4,17 @@ FLAGS = -Wall -pedantic
 
 main : main.o wavefunction.o AngularMomentum.o newtoncotes.o makefile
 	      $(COMP) -o main main.o wavefunction.o AngularMomentum.o newtoncotes.o
-
-main.o : main.cpp wavefunction.hpp makefile
-	        $(COMP) -c $(FLAGS) main.cpp
-
-wavefunction.o : wavefunction.cpp wavefunction.hpp AngularMomentum.hpp newtoncotes.hpp makefile
-	                $(COMP) -c $(FLAGS) wavefunction.cpp
+	      
+newtoncotes.o : newtoncotes.cpp newtoncotes.hpp makefile
+		          $(COMP) -c $(FLAGS) newtoncotes.cpp
 
 AngularMomentum.o : AngularMomentum.cpp AngularMomentum.hpp makefile
 				    $(COMP) -c $(FLAGS) AngularMomentum.cpp
 
-newtoncotes.o : newtoncotes.cpp newtoncotes.hpp makefile
-		          $(COMP) -c $(FLAGS) newtoncotes.cpp
+wavefunction.o : wavefunction.cpp wavefunction.hpp AngularMomentum.hpp newtoncotes.hpp makefile
+	                $(COMP) -c $(FLAGS) wavefunction.cpp
 
+main.o : main.cpp wavefunction.hpp makefile
+	        $(COMP) -c $(FLAGS) main.cpp
 clean:
 		rm  main.o wavefunction.o AngularMomentum.o newtoncotes.o
--- wavefunction.hpp
+++ wavefunction.hpp
@@ -23,7 +23,8 @@
 class WaveFunction {
 
    public:
-      WaveFunction( char* , int, wb );
+      //Functions:
+      WaveFunction( const char*, const char* , int, bool );
       ~WaveFunction();
       float get_impact( int );
       double indi_electron( const char, int );
@@ -35,20 +36,20 @@ class WaveFunction {
       static const int MAXn = 5;
       static const int LEN = 1601;
       static const int LIMIT = 1601; //1001;
-      double r[LEN]; //Lattice points for the radial R_nl functions.
-      double Rad[2][2][5][4][LEN];  //The R_nl function values on target.
-      float b[30];  //holds the values of the impact parameter.
-      std::complex<double> amps[2][30][5][4][7];  //Holds the amplitude values for the 30 impact parameters on the target.
-
-      double int_table[2][5][4][5][4][5][4][5][4]; //Holds the integral table.
+      double r[LEN];                                  //Lattice points for the radial R_nl functions.
+      double Rad[2][2][5][4][LEN];                    //The R_nl functions.
+      float b[30];                                    //holds the values of the impact parameter.
+      std::complex<double> amps[2][30][5][4][7];      //Holds the amplitude values for the 30 impact parameters.
+      double int_table[2][5][4][5][4][5][4][5][4];    //Holds the integral table.
       double int_table_wb[2][5][4][5][4][5][4][5][4]; //Holds the integral table for the Wilken and Bauer version.
-
-      int N_terms_T;  //The number of terms in the wave function on target.
-      int (*terms_T)[2][3];  //Holds the quantum numbers of the terms of the wave function.
-      double* factors_T;  //Holds the prefactors for each term of the wave function.
-      int N_terms_P;  //The number of terms in the wave function on projectile.
-      int (*terms_P)[2][3];  //Holds the quantum numbers of the terms of the wave function.
-      double* factors_P;  //Holds the prefactors for each term of the wave function.      
+      
+      //Storage of ground state information:
+      int N_terms_T;        //number of terms on target.
+      int (*terms_T)[2][3]; //quantum numbers of terms on target.
+      double *factors_T;    //prefactors for terms on target.
+      int N_terms_P;        //number of terms on projectile.
+      int (*terms_P)[2][3]; //quantum numbers of terms on projectile.
+      double *factors_P;    //prefactors for terms on projectile.
 
       //Functions:
       int TP_toint( const char );
@@ -56,23 +57,18 @@ class WaveFunction {
       void readlattice();
       void readradial( const char, const char );
       void readamplitudes( const char, int );
-      void readinput( const char, char* );
+      void readinput( const char, const char* );
       int r_index( double );
       double R( const char, const char, int , int , double );
       double Hep1( double );
       std::complex<double> a( const char, int , int, int, int );
-      double table( const char, int, int, int, int, int, int, int, int );
-      
-            //Here
-      
-      //Modify these later:
       double* integrand( const char, int , int , int , int , int , int , int , int );
-      void generate_integral_table( const, char );
+      void generate_integral_table( const char );
       double* integrand_wb( const char, double , int , int , int , int , int , int , int , int );
       void generate_integral_table_wb( const char, double );
+      double table( const char, int, int, int, int, int, int, int, int );
+      double table_wb( const char, int, int, int, int, int, int, int, int );
       
-      // add TP integral functions
-
 };
 
 #endif
